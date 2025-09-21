fbb9e270f64ccf36acf8a9517687a91c2475e634
Ivan Lavayen <ivanalejolavayen@gmail.com>
2025-09-21 16:53:24 -0300

    agrego tp especia LIBRERIA

---
+++ src/Articulo.java
@@ -0,0 +1,89 @@
+import java.util.ArrayList;
+
+public class Articulo {
+    private String nombreArticulo;
+    private String autor;
+    private double precio;
+    private int cantidadPaginas;
+    private String resumen;
+    private ArrayList<String>generos;
+    private double descuento;
+
+    public Articulo(String nombreArticulo, String autor, double precio,
+                    int cantidadPaginas, String resumen, double descuento) {
+        this.nombreArticulo = nombreArticulo;
+        this.autor = autor;
+        this.precio = precio;
+        this.cantidadPaginas = cantidadPaginas;
+        this.resumen = resumen;
+        this.descuento = descuento;
+        this.generos=new ArrayList<>();
+    }
+
+    public String getNombreArticulo() {
+        return nombreArticulo;
+    }
+
+    public void setNombreArticulo(String nombreArticulo) {
+        this.nombreArticulo = nombreArticulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidadPaginas() {
+        return cantidadPaginas;
+    }
+
+    public void setCantidadPaginas(int cantidadPaginas) {
+        this.cantidadPaginas = cantidadPaginas;
+    }
+
+    public String getResumen() {
+        return resumen;
+    }
+
+    public void setResumen(String resumen) {
+        this.resumen = resumen;
+    }
+
+    public double getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(double descuento) {
+        this.descuento = descuento;
+    }
+    //----------METODOS ESPECIALES ----------------
+
+
+    //metodo para agregar un nuevo genero, controlo repetidos
+    //uso contains para saber si ya esta, no se redefine el equals
+    //porque es un String.
+    public void addGenero(String nuevoGenero){
+        if(!generos.contains(nuevoGenero)){
+            generos.add(nuevoGenero);
+        }
+    }
+
+    //metodo para saber si tiene un genero determinado
+    public boolean tieneGenero(String genero){
+        return generos.contains(genero);
+    }
+
+
+
+}
